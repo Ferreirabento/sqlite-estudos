4c363614077fb8b727b67d8dc8a1028b5a48f522
FerreiraBento <jf6808932@gmail.com>
2026-07-18 20:31:29 -0300

    ajustando para iniciar um projeto voltado para o financeiro

--- python/app.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("banco.db")
-
-cursor = conn.cursor()
-
-cursor.execute("CREATE TABLE alunos ( id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, idade INTEGER) ")
-
-cursor.close()
