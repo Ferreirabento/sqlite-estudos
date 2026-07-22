b6e72b4302063036674d86789da9094904414581
João Ferreira <joao.bento.ramos.ferreira@gmail.coom>
2026-07-22 20:02:31 -0300

    teste de frequencia

--- python/consultar.py
+++ python/consultar.py
@@ -4,9 +4,10 @@ conn = sqlite3.connect("C:/Users/joaob/Documents/sqlite-estudos/banco.db")
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT Sentence, Sentiment FROM financial_sentiment_analysis LIMIT 100;")
+limitar = cursor.execute("SELECT Sentence, Sentiment FROM financial_sentiment_analysis LIMIT 100;")
+#print(limitar.fetchall())
 
-for coisa in cursor.fetchall():
-    print(coisa)
+frequencia = cursor.execute("SELECT distinct(Sentiment), count(*) as qualidade FROM financial_sentiment_analysis GROUP BY 1;")
+print(frequencia.fetchall())
 
 conn.close()
