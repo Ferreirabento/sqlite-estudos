4952d0372dc1c1f365430dacb2eef453f41eba56
João Ferreira <joao.bento.ramos.ferreira@gmail.coom>
2026-07-24 11:32:03 -0300

    usei essa consulta com HAVING para saber quais são as duplicatas

--- scripts/select.sql
+++ scripts/select.sql
@@ -2,3 +2,10 @@
 SELECT DISTINCT(sentiment), COUNT(*) as frequencia 
 FROM financial_sentiment_analysis
 GROUP BY 1;
+
+
+-- para saber quais das linhas são duplicatas
+SELECT sentiment, sentence, COUNT(*) as contagens
+FROM financial_sentiment_analysis
+GROUP by 1, 2
+HAVING COUNT(*) > 1;
